d6a5b2944721039fa321abf56f381cfb763f8f5f
gulkzm <gkazimova.2000@gmail.com>
2025-03-31 23:15:03 +0400

    Created the project
    - Project was created
    - Repository of the project was created
    - Repository was cloned

---
+++ Fitlab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fitlab
+//
+//  Created by Gul Kzm on 28.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
